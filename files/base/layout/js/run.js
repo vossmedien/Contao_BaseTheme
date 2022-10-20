692528e81987cf0edb84520afeaacac905adcea8
Christian Voss <christian@vossmedien.de>
2022-10-20 17:43:29 +0200

    Anpassungen

--- files/base/layout/js/run.js
+++ files/base/layout/js/run.js
@@ -330,17 +330,11 @@ Promise.all(promises)
         /* END */
 
 
-
-         if ($(".accordion-nav").length) {
-             $(".accordion-nav").find("i").click(function (e) {
+        if ($(".accordion-nav").length) {
+            $(".accordion-nav").find("i").click(function (e) {
                 $(this).closest("li").toggleClass("expanded");
             });
-         }
-
-
-
-
-
+        }
 
 
         if ($(".mod_mmenuHtml a.offCanvasBasketOpener").length) {
@@ -390,6 +384,117 @@ Promise.all(promises)
             addPlaceholders();
         }
         /* END */
+
+
+        /* Change Buttons with JS instead of CSS */
+
+
+        $('p.back > a, .widget-submit > button').each(function (index) {
+            $(this).addClass("btn btn-primary");
+        });
+
+        $('.submit_container > input.button').each(function (index) {
+            $(this).addClass("btn btn-lg");
+
+
+            if ($(this).hasClass("next")) {
+                $(this).addClass("btn-primary");
+            }
+
+            if ($(this).hasClass("previous")) {
+                $(this).addClass("btn-outline-primary");
+            }
+
+            if ($(this).hasClass("confirm")) {
+                $(this).addClass("btn-success");
+            }
+        });
+
+        $('.submit_container  button').each(function (index) {
+            $(this).addClass("btn");
+
+            if ($(this).hasClass("button_update") || $(this).hasClass("button_checkout")) {
+                $(this).addClass("btn-outline-primary");
+            }
+        });
+
+        $('.submit_container .submit').each(function (index) {
+            //$(this).addClass("btn btn-primary btn-lg");
+        });
+
+        $('.actions_container .submit').each(function (index) {
+            $(this).addClass("btn btn-primary");
+        });
+
+        $('.filter-toggle-control').each(function (index) {
+            $(this).addClass("btn btn-primary");
+        });
+
+        $('.mod_iso_orderhistory td.link > a').each(function (index) {
+            $(this).addClass("btn btn-sm");
+        });
+
+        $('.mod_iso_orderhistory td.link > a:first-child').each(function (index) {
+            $(this).addClass("btn-primary");
+        });
+
+        $('.mod_iso_orderhistory td.link > a:last-child').each(function (index) {
+            $(this).addClass("btn-secondary");
+        });
+
+        $('.mod_iso_addressbook a.add').each(function (index) {
+            $(this).addClass("btn btn-outline-primary");
+        });
+
+        $('p.empty').each(function (index) {
+            $(this).addClass("alert bg-info");
+        });
+
+        $('p.error').each(function (index) {
+            $(this).addClass("alert bg-danger");
+        });
+
+        $('.widget-radio span.note').each(function (index) {
+            $(this).addClass("alert bg-primary");
+        });
+
+
+        $('.message').each(function (index) {
+            $(this).addClass("alert");
+
+            if ($(this).hasClass("success")) {
+                $(this).addClass("bg-success");
+            }
+
+            if ($(this).hasClass("empty")) {
+                $(this).addClass("bg-primary");
+            }
+
+            if ($(this).hasClass("error")) {
+                $(this).addClass("bg-danger");
+            }
+        });
+
+
+        $('#footerNav ul').each(function (index) {
+            $(this).addClass("list-inline");
+        });
+
+        $('#footerNav ul > li ').each(function (index) {
+            $(this).addClass("list-inline-item");
+        });
+
+        $('#main .mod_article > *:not(.content--element):not(.container):not(.ce_html):not(.mod_catalogMasterView):not(.mod_catalogUniversalView)').each(function (index) {
+            $(this).addClass("container");
+        });
+
+        $('#main > .inside > div[class^="mod_"]:not(.mod_article)').each(function (index) {
+            $(this).addClass("container");
+        });
+
+            $('.formbody:not(.row)').each(function (index) {
+            $(this).addClass("row");
+        });
 
         if ($(".modal").length) {
             $(".modal").appendTo("body");
