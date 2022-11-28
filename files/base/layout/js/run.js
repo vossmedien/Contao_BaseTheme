bf6dc15c07994523f0dc31beefa3dcf312267fc9
vossmedien <christian@vossmedien.de>
2022-11-28 13:30:02 +0100

    Anpassungen

--- files/base/layout/js/run.js
+++ files/base/layout/js/run.js
@@ -242,6 +242,18 @@ Promise.all(promises)
         }
         /* END */
 
+
+        $(".matrix td input").each(function (a, b) {
+            $(b).parent().click(function (a) {
+                "radio" == $(b).attr("type") ? $(b).parent().parent().find("input[type=radio]").each(function (a, c) {
+                    $(c)[0] != $(b)[0] ? $(c).prop("checked", !1) : $(c).prop("checked", !0)
+                }) : "checkbox" == $(b).attr("type") && $(b).parent().parent().find("input[type=checkbox]").each(function (c, d) {
+                    $(d)[0] == $(b)[0] && "TD" == a.target.nodeName && $(d).prop("checked", !$(d).prop("checked"))
+                })
+            })
+        })
+
+
         setTimeout(function () {
             $("body").css('opacity', 1);
         }, 250, true);
