68e9d5f890cd3e65b5e5db5cfafb8eaeaa9a239c
vossmedien <christian@vossmedien.de>
2023-04-28 16:24:10 +0200

    Fixes

--- files/base/layout/js/_elements/ce_rsce_switchingcards.js
+++ files/base/layout/js/_elements/ce_rsce_switchingcards.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
         $(".flipping-card--wrapper").each(function (index) {
             var front = $(this).find(".flipping-card--front");
             var back = $(this).find(".flipping-card--back");
-            var frontHeight = front.height();
-            var backHeight = back.height();
+            var efrontHeight = $(this).find(".flipping-card--front .front--inner");
+            var ebackHeight = $(this).find(".flipping-card--back .back--inner");
+            var frontHeight = efrontHeight.height();
+            var backHeight = ebackHeight.height();
 
             if (backHeight > frontHeight) {
-                front.css("height", backHeight);
+                front.css("height", backHeight + 40);
             } else {
-                back.css("height", frontHeight);
+                back.css("height", frontHeight + 40);
             }
         });
     }
