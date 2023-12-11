5a4caf64e7c25f9435b3806f1a6a8ad2b313b2f1
vossmedien <christian@vossmedien.de>
2023-12-11 13:31:06 +0100

    Anpassungen

--- templates/rsce_slider_config.php
+++ templates/rsce_slider_config.php
@@ -1,7 +1,7 @@
 <?php
 // rsce_my_element_config.php
 return array(
-    'label' => array('Custom | Bilder-Slider', ''),
+    'label' => array('Custom | Bilder-Slider (slider)', ''),
     'types' => array('content'),
     'contentCategory' => 'texts',
     'moduleCategory' => 'miscellaneous',
