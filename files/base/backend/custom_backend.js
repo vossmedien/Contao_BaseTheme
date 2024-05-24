be12aa85abd91a8bbbbf775f369a3cd4b8ddc4ea
vossmedien <christian@vossmedien.de>
2024-05-24 17:44:18 +0200

    anpassungen

--- files/base/backend/custom_backend.js
+++ files/base/backend/custom_backend.js
@@ -37,7 +37,7 @@ function elementMatches(element, selector) {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    var rsceGroups = document.querySelectorAll('.rsce_group:not(.rsce_group_no_legend), .rsce_list, .tl_box');
+    var rsceGroups = document.querySelectorAll('.rsce_group:not(.rsce_group_no_legend), .rsce_list, .tl_box:not(.rsce_group)');
 
 // Die Klasse "collapsed" zu jedem Element hinzufügen, wenn sie nicht bereits vorhanden ist
     rsceGroups.forEach(function (group) {
