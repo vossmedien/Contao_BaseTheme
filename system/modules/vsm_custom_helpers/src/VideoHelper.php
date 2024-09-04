78c4c44d960bc7bd6dd2c481f9e83a4ddbfc32d5
vossmedien <christian@vossmedien.de>
2024-09-04 11:45:55 +0200

    Anpassungen

--- system/modules/vsm_custom_helpers/src/VideoHelper.php
+++ system/modules/vsm_custom_helpers/src/VideoHelper.php
@@ -2,14 +2,22 @@
 
 namespace VSM_HelperFunctions;
 
+use Contao\FilesModel;
 use Contao\System;
 
 class VideoHelper
 {
-    public static function renderVideo($src, $ext, $classes = '')
+    public static function renderVideo($fileId, $ext, $classes = '')
     {
+        // Holen Sie sich das FilesModel-Objekt anhand der ID
+        $fileModel = FilesModel::findByUuid($fileId);
+
+        if ($fileModel === null) {
+            return ''; // Datei nicht gefunden
+        }
+
         $rootDir = System::getContainer()->getParameter('kernel.project_dir');
-        $filePath = $rootDir . '/files/' . $src;
+        $filePath = $rootDir . '/' . $fileModel->path;
         $baseDir = dirname($filePath);
         $fileName = pathinfo($filePath, PATHINFO_FILENAME);
 
@@ -20,18 +28,27 @@ class VideoHelper
         foreach ($formatOrder as $format) {
             $potentialFile = $baseDir . '/' . $fileName . '.' . $format;
             if (file_exists($potentialFile)) {
-                $sources[] = "<source type='video/$format' data-src='{{file::$src}}." . $format . "'>";
+                // Suche nach dem FilesModel für diese Datei
+                $potentialFileModel = FilesModel::findByPath(str_replace($rootDir . '/', '', $potentialFile));
+                if ($potentialFileModel !== null) {
+                    $sources[] = "<source type='video/$format' src='" . $potentialFileModel->path . "'>";
+                }
             }
         }
 
         // Wenn keine alternativen Formate gefunden wurden, verwende das ursprüngliche Format
         if (empty($sources)) {
-            $sources[] = "<source type='video/$ext' data-src='{{file::$src}}'>";
+            $sources[] = "<source type='video/$ext' src='" . $fileModel->path . "'>";
         }
 
         $sourceString = implode("\n        ", $sources);
 
-        return "<video class='lazy $classes' autoplay muted loop playsinline data-src='{{file::$src}}'>
+        // Verwende das erste gefundene Format für das data-src Attribut des Video-Tags
+        $firstSource = reset($sources);
+        preg_match('/src=\'([^\']+)\'/', $firstSource, $matches);
+        $dataSrc = $matches[1] ?? $fileModel->path;
+
+        return "<video class='lazy $classes' autoplay muted loop playsinline data-src='$dataSrc'>
         $sourceString
     </video>";
     }
@@ -41,5 +58,4 @@ class VideoHelper
         $videoFormats = ['mp4', 'webm', 'ogg', 'mov']; // Fügen Sie hier weitere Videoformate hinzu, falls nötig
         return in_array(strtolower($extension), $videoFormats);
     }
-
 }
