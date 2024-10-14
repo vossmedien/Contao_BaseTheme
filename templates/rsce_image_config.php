5086713e806611df3c2a77e256ef052c8b8eb467
vossmedien <christian@vossmedien.de>
2024-10-14 15:41:00 +0200

    Anpassungen

---
+++ templates/rsce_image_config.php
@@ -0,0 +1,63 @@
+<?php
+
+use VSM_HelperFunctions\ButtonHelper;
+use VSM_HelperFunctions\GlobalElementConfig;
+
+//rsce_my_element_config.php
+return array(
+    'label' => array('Custom | Bild (image)', ''),
+    'types' => array('content'),
+    'contentCategory' => 'Custom',
+    'moduleCategory' => 'miscellaneous',
+    'standardFields' => array('cssID'),
+    'wrapper' => array(
+        'type' => 'none',
+    ),
+    'fields' => array(
+
+        'topline' => array(
+            'label' => array('Topline', 'Text oberhalb der Überschrift'),
+            'inputType' => 'text',
+            'eval' => array('tl_class' => 'w50', 'allowHtml' => true),
+        ),
+        'subline' => array(
+            'label' => array('Subline', 'Text unterhalb der Überschrift'),
+            'inputType' => 'text',
+            'eval' => array('tl_class' => 'w50', 'allowHtml' => true),
+        ),
+
+
+        'animation_type' => array(
+            'label' => array(
+                'de' => array('Art der Einblendeanimation', 'Siehe https://animate.style/ für Beispiele'),
+            ),
+            'inputType' => 'select',
+            'options' => GlobalElementConfig::getAnimations(),
+            'eval' => array('chosen' => 'true')
+        ),
+
+        'image' => array(
+            'label' => array('Bild', ''),
+            'inputType' => 'fileTree',
+            'eval' => array(
+                'multiple' => false,
+                'fieldType' => 'radio',
+                'filesOnly' => true,
+                'extensions' => 'jpg,jpeg,png,svg,tif',
+            ),
+        ),
+
+        'size' => array(
+            'label' => array('Bildgröße', 'Hier können Sie die Abmessungen des Bildes und den Skalierungsmodus festlegen.'),
+            'inputType' => 'imageSize',
+            'options' => \Contao\System::getContainer()->get('contao.image.sizes')->getAllOptions(),
+            'reference' => &$GLOBALS['TL_LANG']['MSC'],
+            'eval' => array(
+                'rgxp' => 'digit',
+                'includeBlankOption' => true,
+                'tl_class' => ' clr'
+            ),
+        ),
+
+    ),
+);
