c9a3ac4deb86e730e7360bb93d25c84fca090b12
christian.voss <christian@vossmedien.de>
2025-04-24 09:23:08 +0200

    Anpassungen

--- templates/rsce_services_overview_config.php
+++ templates/rsce_services_overview_config.php
@@ -1,6 +1,7 @@
 <?php
 
 use Vsm\VsmHelperTools\Helper\GlobalElementConfig;
+use Vsm\VsmHelperTools\Helper\ButtonHelper;
 
 return array(
     'label' => array('Custom | Leistungen im Überblick (services_overview)', ''),
@@ -23,12 +24,21 @@ return array(
             'eval' => array('tl_class' => 'w50', 'allowHtml' => true),
         ),
         'animation_type' => array(
-            'label' => array(
-                'de' => array('Art der Einblendeanimation', 'Siehe https://animate.style/ für Beispiele'),
-            ),
+            'label' => array('Animation der Liste', 'Art der Einblendeanimation für die Listenelemente (links). Siehe https://animate.style/'),
             'inputType' => 'select',
             'options' => GlobalElementConfig::getAnimations(),
-            'eval' => array('chosen' => 'true', 'tl_class' => 'clr')
+            'eval' => array('chosen' => true, 'tl_class' => 'w50')
+        ),
+        'disable_hover' => array(
+            'label' => array('Interaktion per Klick', 'Wenn aktiviert, wird der Inhalt rechts nur bei Klick auf ein Listenelement gewechselt (statt bei Mouseover).'),
+            'inputType' => 'checkbox',
+            'eval' => array('tl_class' => 'w50 m12'),
+        ),
+        'show_arrow' => array(
+            'label' => array('Pfeil anzeigen', 'Blendet den Pfeil (->) vor den Titeln in der linken Liste ein.'),
+            'inputType' => 'checkbox',
+            'default' => true, // Standardmäßig Pfeil anzeigen
+            'eval' => array('tl_class' => 'w50 m12'),
         ),
         'size' => array(
             'label' => array('Bildgröße', 'Hier können Sie die Abmessungen des Bildes und den Skalierungsmodus festlegen.'),
@@ -42,34 +52,116 @@ return array(
             ),
         ),
         'services' => array(
-            'label' => array('Leistungen', ''),
+            'label' => array('Leistungen', 'Definieren Sie hier die einzelnen Leistungen, die angezeigt werden sollen.'),
             'elementLabel' => '%s. Leistung',
             'inputType' => 'list',
             'minItems' => 1,
             'maxItems' => 20,
             'fields' => array(
                 'title' => array(
-                    'label' => array('Titel', ''),
+                    'label' => array('Titel', 'Angezeigter Titel in der linken Liste.'),
                     'inputType' => 'text',
-                    'eval' => array('mandatory' => true, 'tl_class' => 'w50'),
+                    'eval' => array('mandatory' => true, 'tl_class' => 'w50 clr'),
                 ),
                 'link' => array(
-                    'label' => array('Link', ''),
+                    'label' => array('Link', 'Optionaler Link für das Listenelement.'),
                     'inputType' => 'url',
-                    'eval' => array('mandatory' => true, 'tl_class' => 'w50'),
+                    'eval' => array('mandatory' => false, 'tl_class' => 'w50'),
                 ),
+                'content_headline' => array(
+                    'label' => array('Überschrift', 'Überschrift im rechten Inhaltsbereich, wenn dieses Element aktiv ist.'),
+                    'inputType' => 'text',
+                     'eval' => array('tl_class' => 'w50 clr'),
+                ),
+                 'content_hl' => array(
+                     'label' => array('Typ der Überschrift', ''),
+                     'inputType' => 'select',
+                     'options' => array(
+                         'h1' => 'H1',
+                         'h2' => 'H2',
+                         'h3' => 'H3',
+                         'h4' => 'H4',
+                         'h5' => 'H5',
+                         'h6' => 'H6',
+                     ),
+                     'eval' => array('includeBlankOption' => true, 'tl_class' => 'w50'),
+                 ),
+                'content_topline' => array(
+                    'label' => array('Topline', 'Text oberhalb der Überschrift im rechten Inhaltsbereich.'),
+                    'inputType' => 'text',
+                    'eval' => array('tl_class' => 'w50 clr', 'allowHtml' => true),
+                ),
+                'content_subline' => array(
+                    'label' => array('Subline', 'Text unterhalb der Überschrift im rechten Inhaltsbereich.'),
+                    'inputType' => 'text',
+                    'eval' => array('tl_class' => 'w50', 'allowHtml' => true),
+                ),
+                 'headline_animation_type' => array(
+                     'label' => array('Animation der Überschrift', 'Animation für Topline, Überschrift und Subline.'),
+                     'inputType' => 'select',
+                     'options' => GlobalElementConfig::getAnimations(),
+                     'eval' => array('chosen' => true, 'tl_class' => 'w50 clr')
+                 ),
                 'image' => array(
-                    'label' => array('Bild', ''),
+                    'label' => array('Bild', 'Optionales Bild im rechten Inhaltsbereich.'),
                     'inputType' => 'fileTree',
                     'eval' => array(
                         'filesOnly' => true,
-                        'extensions' => 'jpg,jpeg,png,gif,svg',
+                        'extensions' => Contao\Config::get('validImageTypes'),
                         'fieldType' => 'radio',
-                        'mandatory' => true,
-                        'tl_class' => 'clr'
+                        'mandatory' => false,
+                        'tl_class' => 'clr',
+                        'submitOnChange' => true // Wichtig für Subpalette
                     ),
                 ),
+                 'image_animation_type' => array(
+                     'label' => array('Animation des Bildes', ''),
+                     'inputType' => 'select',
+                     'options' => GlobalElementConfig::getAnimations(),
+                     'eval' => array('chosen' => true, 'tl_class' => 'w50 clr')
+                 ),
+                'content_text' => array(
+                    'label' => array('Text', 'Optionaler Text im rechten Inhaltsbereich (unterhalb des Bildes).'),
+                    'inputType' => 'textarea',
+                    'eval' => array('rte' => 'tinyMCE', 'tl_class' => 'clr'),
+                ),
+                 'text_animation_type' => array(
+                     'label' => array('Animation des Textes', ''),
+                     'inputType' => 'select',
+                     'options' => GlobalElementConfig::getAnimations(),
+                     'eval' => array('chosen' => true, 'tl_class' => 'w50 clr')
+                 ),
+                'buttons' => array(
+                    'label' => array('Buttons', 'Optionale Buttons im rechten Inhaltsbereich (unterhalb des Textes).'),
+                    'elementLabel' => '%s. Button',
+                    'inputType' => 'list',
+                    'minItems' => 0,
+                    'maxItems' => 20,
+                    'eval' => array('tl_class' => 'clr'),
+                    'fields' => ButtonHelper::getButtonConfig(),
+                ),
             ),
+            'subpalettes' => array(
+                 'image' => 'size,open_lightbox', // Zeige diese Felder nur wenn 'image' ausgewählt ist
+            ),
+            'fields_for_subpalettes' => array(
+                 'size' => array(
+                    'label' => array('Bildgröße', 'Hier können Sie die Abmessungen des Bildes und den Skalierungsmodus festlegen.'),
+                    'inputType' => 'imageSize',
+                    'options' => Contao\System::getContainer()->get('contao.image.sizes')->getAllOptions(),
+                    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+                    'eval' => array(
+                        'rgxp' => 'digit',
+                        'includeBlankOption' => true,
+                         'tl_class' => 'w50'
+                    ),
+                ),
+                'open_lightbox' => array(
+                    'label' => array('Bild in Lightbox öffnen', ''),
+                    'inputType' => 'checkbox',
+                    'eval' => array('tl_class' => 'w50 m12'),
+                ),
+            )
         ),
     ),
 );
--- vendor/vsm/vsm-helper-tools/src/Helper/HeadlineHelper.php
+++ vendor/vsm/vsm-helper-tools/src/Helper/HeadlineHelper.php
@@ -31,10 +31,10 @@ class HeadlineHelper
         $headline = $headline ?? '';
         $subline = $subline ?? '';
         $headlineType = $headlineType ?? 'h2';
-        $animationType = $animationType ?? 'animate__fadeInUp';
+        $animationType = $animationType ?? 'animate__fadeIn';
         $textColor = $textColor ?? '';
         $headlineClass = $headlineClass ?? '';
-        
+
         $textColorStyle = $textColor ? "color:{$textColor};" : '';
         $headlineClassAttribute = "class=\"ce--headline hl-{$headlineType}" . ($headlineClass ? " {$headlineClass}" : '') . "\"";
 
@@ -71,3 +71,4 @@ class HeadlineHelper
         return $headlineHTML;
     }
 }
+
