1262fde38ea54ebb51c67b94d2962a15e32d55f2
christian.voss <christian@vossmedien.de>
2025-06-04 12:19:33 +0200

    Anpassungen

---
+++ vendor/vsm/vsm-news-events-enhancements/src/Resources/contao/languages/de/tl_calendar_events.php
@@ -0,0 +1,18 @@
+<?php
+
+// contao/languages/de/tl_calendar_events.php
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['isSoldOut'] = ['Ausgebucht', 'Veranstaltung ist ausgebucht'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['eventTime'] = ['Veranstaltungsbeginn', 'Uhrzeit der Veranstaltung'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['priceAdvance'] = ['Vorverkaufspreis', 'Preis im Vorverkauf'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['priceEvening'] = ['Abendkassenpreis', 'Preis an der Abendkasse'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['ticketUrl'] = ['Ticket-Link', 'URL zum Ticketverkauf'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['additionalUrls'] = ['Zusätzliche URLs', 'Weitere relevante Links'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['headerImage'] = ['Kopfbild', 'Wählen Sie ein Kopfbild aus.'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['headerImageSize'] = ['Bildgröße Kopfbild', 'Größe des Kopfbildes.'];
+$GLOBALS['TL_LANG']['tl_calendar_events']['longDescription'] = ['Langbeschreibung', 'Ausführliche Beschreibung der Veranstaltung.'];
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['time_legend'] = 'Zeitangaben';
+$GLOBALS['TL_LANG']['tl_calendar_events']['price_legend'] = 'Preise';
+$GLOBALS['TL_LANG']['tl_calendar_events']['link_legend'] = 'Links';
+$GLOBALS['TL_LANG']['tl_calendar_events']['teaser_legend'] = 'Teaser-Text'; 
