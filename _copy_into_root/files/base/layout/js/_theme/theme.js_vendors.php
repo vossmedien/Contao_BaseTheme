8400f207063507790472105d4a6b953cbbbebe76
vossmedien <christian@vossmedien.de>
2025-07-08 00:48:54 +0200

    Anpassungen

--- _copy_into_root/files/base/layout/js/_theme/theme.js_vendors.php
+++ _copy_into_root/files/base/layout/js/_theme/theme.js_vendors.php
@@ -1,11 +1,11 @@
 <?php
-// files/base/layout/js/_kgdental/theme.js_vendors.php
+// files/base/layout/js/_theme/theme.js_vendors.php
 return [
-  [ 'src' => 'js-cookie/dist/js.cookie.min.js', 'attributes' => '|static' ],
-  [ 'src' => 'bootstrap/dist/js/bootstrap.bundle.min.js', 'attributes' => '|defer|static' ],
-  [ 'src' => 'venobox/dist/venobox.min.js', 'attributes' => '|defer|static' ],
+  [ 'src' => 'js-cookie/dist/js.cookie.min.js', 'attributes' => '|static' ], // Immer benötigt für Cookies
+  [ 'src' => 'bootstrap/dist/js/bootstrap.bundle.min.js', 'attributes' => '|defer|static' ], // Immer benötigt für Tooltips/Modals
+  [ 'src' => 'venobox/dist/venobox.min.js', 'attributes' => '|defer' ], // Entfernt |static - wird bedingt geladen
   //[ 'src' => '@popperjs/core/dist/umd/popper.min.js', 'attributes' => '|defer|static' ],
-  [ 'src' => 'swiper/swiper-bundle.min.js', 'attributes' => '|defer|static' ],
-  [ 'src' => 'macy/dist/macy.js', 'attributes' => '|defer|static' ],
-  [ 'src' => 'mmenu-light/dist/mmenu-light.js', 'attributes' => '|defer|static' ]
+  [ 'src' => 'swiper/swiper-bundle.min.js', 'attributes' => '|defer' ], // Entfernt |static - wird bedingt geladen
+  [ 'src' => 'macy/dist/macy.js', 'attributes' => '|defer|static' ], // Bleibt static - relativ klein
+  [ 'src' => 'mmenu-light/dist/mmenu-light.js', 'attributes' => '|defer|static' ] // Bleibt static - Mobile Navigation
 ]; 
